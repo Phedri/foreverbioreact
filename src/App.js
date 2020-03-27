aaf6b704b51118f59cb7a43b6caf632e13c05dd4
Imane <imaneelmouradi98@gmail.com>
2020-03-27 20:58:42 +0100

    add: home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./css/App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
